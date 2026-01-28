22b692b71277d4eb036f46d99b563fd78857684f
shubhangi7203 <94372711+shubhangi7203@users.noreply.github.com>
2026-01-28 16:55:02 +0530

    Update TestBase.java

--- src/test/java/com/qa/testscript/TestBase.java
+++ src/test/java/com/qa/testscript/TestBase.java
@@ -20,7 +20,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
